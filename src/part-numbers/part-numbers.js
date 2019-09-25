f7516d3832abf98d3ee45e692ce0acd5bd3ec4c3
Adam Lee <adam.lee@activatedresearch.com>
2019-09-25 15:16:14 -0500

    show contact info on pat numbers page

--- src/part-numbers/part-numbers.js
+++ src/part-numbers/part-numbers.js
@@ -9,10 +9,21 @@ function partNumbers() {
   }
 
   return {
-    view: () => m('div.pl3.pt2.h-100',
+    view: () => m('div.relative.pl3.pt2.h-100',
       [
         m('div.f4.b.mb3', 'These are the part numbers for your purchase order. Please review your selections to ensure your PO is correct.'), // TODO make this a component,
         m('div', getPartNumbers()),
+        m('div.f6.bottom-1.absolute', [
+          m('div.pb1', 'Please contact us if you have any questions.'),
+          m('div.flex.pb1', [
+            m('div.w-20', 'email:'),
+            m('a', { href: 'mailto: contact@activatedresearch.com' }, 'contact@activatedresearch.com'),
+          ]),
+          m('div.flex', [
+            m('div.w-20', 'phone:'),
+            m('a', { href: 'tel:+1612-787-2721' }, '(612) 787-2721'),
+          ]),
+        ]),
       ]
     ),
   };
