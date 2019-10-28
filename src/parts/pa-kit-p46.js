69b9237f197912f1a3bcf802926fda34b9676d22
Adam Lee <adam.lee@activatedresearch.com>
2019-10-28 15:43:56 -0500

    configure agilent 6890

--- src/parts/pa-kit-p46.js
+++ src/parts/pa-kit-p46.js
@@ -1,9 +1,10 @@
 const frontOrBackInlet = require('../question-tree/question/installation-position/answers/front-or-back-inlet');
+const auxillaryDetector = require('../question-tree/question/installation-position/answers/auxillary-detector');
 const packed = require('../question-tree/question/column/answers/packed');
 
 const part = {
   number: 'PA-KIT-P46',
-  applies: () => frontOrBackInlet.isSelected && packed.isSelected,
+  applies: () => (frontOrBackInlet.isSelected || auxillaryDetector.isSelected) && packed.isSelected,
 };
 
 module.exports = part;
--- src/parts/pa-sub-42z.js
+++ src/parts/pa-sub-42z.js
@@ -1,9 +1,10 @@
 const frontOrBackInlet = require('../question-tree/question/installation-position/answers/front-or-back-inlet');
+const auxillaryDetector = require('../question-tree/question/installation-position/answers/auxillary-detector');
 const capillary = require('../question-tree/question/column/answers/capillary');
 
 const part = {
   number: 'PA-SUB-42Z',
-  applies: () => frontOrBackInlet.isSelected && capillary.isSelected,
+  applies: () => (frontOrBackInlet.isSelected || auxillaryDetector.isSelected) && capillary.isSelected,
 };
 
 module.exports = part;
