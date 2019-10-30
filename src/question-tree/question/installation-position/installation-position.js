bd48df7c77809ffcaf830cf9b6c24e86f68411d7
Adam Lee <adam.lee@activatedresearch.com>
2019-10-30 15:31:09 -0500

    capitalize polyarc

--- src/question-tree/question/installation-position/installation-position.js
+++ src/question-tree/question/installation-position/installation-position.js
@@ -3,7 +3,7 @@ const gc = require('../gc/gc');
 
 const question = {
   title: "Installation Position",
-  text: "Where will the polyarc be installed?",
+  text: "Where will the Polyarc be installed?",
   allAnswers: answers,
   isValid: function() {
     return !this.answer && gc.answer;
